584eb8b5d6bd171000b346274cfa2ef824f86011
Brent <brent.axthelm@gmail.com>
2017-03-06 10:37:10 -0500

    Force tar to use local file. Escape jar path in regex when capturing PID.

--- src/test/scala/com/localytics/sbt/dynamodb/PidUtilsTest.scala
+++ src/test/scala/com/localytics/sbt/dynamodb/PidUtilsTest.scala
@@ -8,32 +8,36 @@ import org.scalatest.Matchers
 class PidUtilsTest extends FunSpec with Matchers {
 
   describe("PidUtils") {
+    val jarPath = if (PidUtils.osName.toLowerCase.contains("windows"))
+      "C:\\Users\\person\\code\\repository\\aws-mocks\\.dynamo-db\\DynamoDBLocal.jar"
+    else
+      "/Users/person/code/repository/aws-mocks/.dynamo-db/DynamoDBLocal.jar"
 
     it("should extract PID correctly") {
       val jpsOutput =
-        """
-          |92433 sun.tools.jps.Jps -ml
-          |88818 /usr/local/Cellar/sbt/0.13.7/libexec/sbt-launch.jar
-          |88421 /usr/local/Cellar/sbt/0.13.7/libexec/sbt-launch.jar
-          |5895 com.intellij.database.remote.RemoteJdbcServer com.mysql.jdbc.Driver
-          |92431 /Users/person/code/repository/aws-mocks/dynamo-db/DynamoDBLocal.jar -port 8000 -inMemory -sharedDb
-          |74414 /usr/local/Cellar/sbt/0.13.7/libexec/sbt-launch.jar
+        s"""
+           |92433 sun.tools.jps.Jps -ml
+           |88818 /usr/local/Cellar/sbt/0.13.7/libexec/sbt-launch.jar
+           |88421 /usr/local/Cellar/sbt/0.13.7/libexec/sbt-launch.jar
+           |5895 com.intellij.database.remote.RemoteJdbcServer com.mysql.jdbc.Driver
+           |92431 $jarPath -port 8000 -inMemory -sharedDb
+           |74414 /usr/local/Cellar/sbt/0.13.7/libexec/sbt-launch.jar
         """.stripMargin
-      val jar = new File("/Users/person/code/repository/aws-mocks/dynamo-db/DynamoDBLocal.jar")
+      val jar = new File(jarPath)
       PidUtils.extractPid(jpsOutput, 8000, jar) should equal(Some("92431"))
     }
 
     it("should resolve multiple local dynamodb instances") {
       val jpsOutput =
-        """
-          |92433 sun.tools.jps.Jps -ml
-          |92430 /Users/person/code/macro/repository/aws-mocks/dynamo-db/DynamoDBLocal.jar -port 8001 -inMemory -sharedDb
-          |92433 /Users/person/code/mono/repository/aws-mocks/dynamo-db/DynamoDBLocal.jar -port 8002 -inMemory -sharedDb
-          |5895 com.intellij.database.remote.RemoteJdbcServer com.mysql.jdbc.Driver
-          |92431 /Users/person/code/repository/aws-mocks/dynamo-db/DynamoDBLocal.jar -port 8000 -inMemory -sharedDb
-          |74414 /usr/local/Cellar/sbt/0.13.7/libexec/sbt-launch.jar
+        s"""
+           |92433 sun.tools.jps.Jps -ml
+           |92430 $jarPath -port 8001 -inMemory -sharedDb
+           |92433 $jarPath -port 8002 -inMemory -sharedDb
+           |5895 com.intellij.database.remote.RemoteJdbcServer com.mysql.jdbc.Driver
+           |92431 $jarPath -port 8000 -inMemory -sharedDb
+           |74414 /usr/local/Cellar/sbt/0.13.7/libexec/sbt-launch.jar
         """.stripMargin
-      val jar = new File("/Users/person/code/repository/aws-mocks/dynamo-db/DynamoDBLocal.jar")
+      val jar = new File(jarPath)
       PidUtils.extractPid(jpsOutput, 8000, jar) should equal(Some("92431"))
 
     }
